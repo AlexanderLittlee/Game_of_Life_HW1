229345fefb222bc9197c1a1664045996378ea787
sandorkiss@stud.uni-obuda.hu <sandorkiss@stud.uni-obuda.hu>
2023-03-29 23:45:53 +0200

    Game header filled with constructor and function definitions

--- Game_of_Life_HW1/Game.h
+++ Game_of_Life_HW1/Game.h
@@ -1,5 +1,38 @@
 #pragma once
+#include <vector>
+using namespace std;
 
-class Game {
+class game {
+
+public:
+	class cell {
+
+	public:
+
+		cell() = default;
+		cell(const bool& alive);
+
+	private:
+		
+		int mNeighbours=0;
+		bool mAlive=false;
+	};
+
+public:
+
+	game(const int& sideA);
+	game(const int& sideA, const int& sideB);
+	game(const int& sideA, const float& Chance);
+	game(const int& sideA, const int& sideB, const float& Chance);
+	game(const int& height, const int& width, const int& top, const int& left, const vector<vector<cell>>& vect);
+
+
+private:
+
+	int mSideA;
+	int mSideB=0;
+	float mChance = 0.1;
+	vector<vector<cell>> mDish;
+	bool mOver = false;
 
 };
